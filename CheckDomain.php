eaa964e0bd007775d9f3fd0268ae51088f89c696
nastia <vartan.anastasia@ya.ru>
2017-09-07 14:58:08 +0300

    sms in half an hour and exception in guzzle

--- CheckDomain.php
+++ CheckDomain.php
@@ -1,5 +1,6 @@
 <?
 namespace Panel;
+use MongoDB\BSON\Timestamp;
 
 
 /**
@@ -20,9 +21,14 @@ class CheckDomain{
     const INTERVAL = 5;
 
     /**
+     * time delta for send sms
+     */
+    const DELTA = 1800;
+
+    /**
      * phone to send sms to inform
      */
-    const SMS_INFORM_NUMBER = '79104**1903';
+    const SMS_INFORM_NUMBER = '7910****903';
 
     /**
      * Worker
@@ -71,10 +77,15 @@ class CheckDomain{
      */
     private function checkDomain($url)
     {
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', $url);
+        try {
+            $client = new \GuzzleHttp\Client();
+            $response = $client->request('GET', $url);
+            $status = $response->getStatusCode();
+        }catch (\GuzzleHttp\Exception\ConnectException  $e){
+            $status = 404;
+        }
 
-        return $response->getStatusCode();
+        return $status;
     }
 
 
@@ -129,16 +140,52 @@ class CheckDomain{
                 $inform_arr[$s["UF_DOMAIN"]] += 1;
         }
 
+        $all_informs = self::last_inform();
         foreach ($inform_arr as $key => $i){
             if ($i >= 5){
-                $message = "Сайт {$domains[$key]["UF_DOMAIN"]} не доступен";
-                $sms_aero_user = \Lexand\Hiload::GetHLItemsByID(\Lexand\Helper::SMS_AERO_USER)[0];
-                $send = new \SMS\Client($sms_aero_user['UF_LOGIN'], $sms_aero_user['UF_PASSWORD']);
-                $sms = new \SMS\SMS($message, self::SMS_INFORM_NUMBER);
-                $send->send_sms($sms, \SMS\Client::TYPE_7);
+                $send_sms = true;
+                foreach ($all_informs[$key] as $domain_sms){
+                    $delta = time() - MakeTimeStamp($domain_sms["UF_DATE"]);
+                    if ($delta<=self::DELTA){
+                        $send_sms = false;
+                        break;
+                    }
+                }
+                if ($send_sms) {
+                    $message = "Сайт {$domains[$key]["UF_DOMAIN"]} не доступен";
+                    $sms_aero_user = \Lexand\Hiload::GetHLItemsByID(\Lexand\Helper::SMS_AERO_USER)[0];
+                    $send = new \SMS\Client($sms_aero_user['UF_LOGIN'], $sms_aero_user['UF_PASSWORD']);
+                    $sms = new \SMS\SMS($message, self::SMS_INFORM_NUMBER);
+                    $send->send_sms($sms, \SMS\Client::TYPE_7);
+
+                    # log sms
+                    $log_sms = \Lexand\Hiload::GetHLEntityClass(\Lexand\Helper::PANEL_INFORM_SMS);
+                    $log_sms::add(
+                        [
+                            'UF_DOMAIN' => $key,
+                            'UF_PHONE' => self::SMS_INFORM_NUMBER,
+                            'UF_SMS_TEXT' => $message,
+                            'UF_DATE' => date("d.m.Y H:i:s")
+                        ]
+                    );
+                }
             }
-            break;
         }
         return;
+    }
+
+
+    /**
+     * all sms by domain
+     *
+     * @return array
+     */
+    private function last_inform(){
+        $sms = \Lexand\Hiload::GetHLItemsByID(\Lexand\Helper::PANEL_INFORM_SMS);
+        $domain_sms = [];
+        foreach ($sms as $s){
+            $domain_sms[$s["UF_DOMAIN"]][] = $s;
+        }
+        return $domain_sms;
     }
 }
